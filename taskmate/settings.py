2c7cb67fc33a7a62ee5e16a75dd4aba2b9c3e167
shiva <shivabalan.balakrishnan175@gmail.com>
2020-04-24 22:49:39 +0530

    changed successfully

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -113,9 +113,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles') 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ['/pydjango/django-project/taskmate/taskmate/static']
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles') 
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
