d9cbb4d944cdf365e64e257fc5cf68509d6a6bb4
shiva <shivabalan.balakrishnan175@gmail.com>
2020-04-24 20:14:12 +0530

    updated requirements file

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ['/pydjango/django-project/taskmate/taskmate/static']
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles') 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
